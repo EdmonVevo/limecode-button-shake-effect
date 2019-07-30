4a863790ed16a1b881c068b9797c872e4ff1200d
Edmon <edmonvevo@gmail.com>
2019-07-31 01:25:38 +0400

    Revert "Ready"
    This reverts commit 26f9004be73afe8b7c37ef86fd44d79cfc9ce553.

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(()=>{
-	$('.button').on('mouseenter',function(){
-		$(this).addClass('button_hover');
-	})
-
-	$('.button').on('mouseleave',function(){
-		$(this).removeClass('button_hover');
-	})
-})
