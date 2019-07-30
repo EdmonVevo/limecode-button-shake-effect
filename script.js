7c2fa88e1f97de9a93141b6182adef79564825d9
Edmon <edmonvevo@gmail.com>
2019-07-31 01:28:22 +0400

    Ready

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+	$('.button').on('mouseenter',function(){
+		$(this).addClass('button_hover');
+	})
+
+	$('.button').on('mouseleave',function(){
+		$(this).removeClass('button_hover');
+	})
+}) 
